cb5fa875c191e002558cb1646536f27271e988ae
Nurisa <nurisa.abdykalykova03@gmail.com>
2023-04-04 12:14:17 +0600

    first commit

---
+++ src/main/java/com/example/gadgetariumb8/GadgetariumB8Application.java
@@ -0,0 +1,13 @@
+package com.example.gadgetariumb8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GadgetariumB8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GadgetariumB8Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gadgetariumb8/GadgetariumB8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gadgetariumb8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GadgetariumB8ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
