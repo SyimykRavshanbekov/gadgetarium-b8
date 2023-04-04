7d6753efd16b90cf1ba83d6ff583c9886601899a
Nurisa <nurisa.abdykalykova03@gmail.com>
2023-04-04 12:25:32 +0600

    add introduction html file

--- src/main/java/com/example/gadgetariumb8/GadgetariumB8Application.java
+++ src/main/java/com/example/gadgetariumb8/GadgetariumB8Application.java
@@ -2,12 +2,20 @@ package com.example.gadgetariumb8;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@Controller
 public class GadgetariumB8Application {
 
     public static void main(String[] args) {
         SpringApplication.run(GadgetariumB8Application.class, args);
+    }
+
+    @GetMapping
+    String introduction(){
+        return "introduction";
     }
 
 }
