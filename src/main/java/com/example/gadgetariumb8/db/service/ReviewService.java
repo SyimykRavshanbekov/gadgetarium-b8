d3206e391d490d907198cb4972e9f7b9b75aac72
Kurstan <kurstanerkinbaev2@gmail.com>
2023-04-24 18:05:34 +0300

    did clean code

--- src/main/java/com/example/gadgetariumb8/db/service/ReviewService.java
+++ src/main/java/com/example/gadgetariumb8/db/service/ReviewService.java
@@ -6,8 +6,11 @@ import com.example.gadgetariumb8.db.dto.response.SimpleResponse;
 import java.util.List;
 
 public interface ReviewService {
+
     SimpleResponse deleteById(Long id);
-    SimpleResponse replyToFeedback(String answer,Long id);
-    List<ReviewResponse>getAllReview(String param);
+
+    SimpleResponse replyToFeedback(String answer, Long id);
+
+    List<ReviewResponse> getAllReview(String param);
 
 }
