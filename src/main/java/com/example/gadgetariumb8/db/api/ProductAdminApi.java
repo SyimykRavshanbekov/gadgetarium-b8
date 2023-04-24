787da4c1c61621b29ddd639725cae0547f2094b5
Nurisa <nurisa.abdykalykova03@gmail.com>
2023-04-24 19:00:36 +0600

    test

--- src/main/java/com/example/gadgetariumb8/db/api/ProductAdminApi.java
+++ src/main/java/com/example/gadgetariumb8/db/api/ProductAdminApi.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.*;
 public class ProductAdminApi {
     private final ProductService productService;
 
-    @PostMapping
+    @PostMapping("/add-product")
     @Operation(summary = "To save the product.", description = "This method to save the product.")
     @PreAuthorize("hasAuthority('ADMIN')")
     public SimpleResponse saveProduct(@RequestBody @Valid ProductRequest productRequest) {
