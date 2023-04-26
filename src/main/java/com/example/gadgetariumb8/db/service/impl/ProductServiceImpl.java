85dccece5a7f4649a7112ea1af6bc75cee6987cd
kutman <kkutman18@gmail.com>
2023-04-26 20:29:21 +0600

    compare Product

--- src/main/java/com/example/gadgetariumb8/db/service/impl/ProductServiceImpl.java
+++ src/main/java/com/example/gadgetariumb8/db/service/impl/ProductServiceImpl.java
@@ -2,26 +2,44 @@ package com.example.gadgetariumb8.db.service.impl;
 
 import com.example.gadgetariumb8.db.dto.request.ProductRequest;
 import com.example.gadgetariumb8.db.dto.request.SubProductRequest;
+import com.example.gadgetariumb8.db.dto.response.CompareProductResponse;
 import com.example.gadgetariumb8.db.dto.response.SimpleResponse;
 import com.example.gadgetariumb8.db.exception.exceptions.NotFoundException;
-import com.example.gadgetariumb8.db.model.Brand;
-import com.example.gadgetariumb8.db.model.Product;
-import com.example.gadgetariumb8.db.model.SubCategory;
-import com.example.gadgetariumb8.db.model.SubProduct;
+import com.example.gadgetariumb8.db.model.*;
 import com.example.gadgetariumb8.db.repository.BrandRepository;
 import com.example.gadgetariumb8.db.repository.SubCategoryRepository;
 import com.example.gadgetariumb8.db.repository.SubProductRepository;
+import com.example.gadgetariumb8.db.repository.UserRepository;
 import com.example.gadgetariumb8.db.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ProductServiceImpl implements ProductService {
     private final SubCategoryRepository subCategoryRepository;
     private final BrandRepository brandRepository;
     private final SubProductRepository subProductRepository;
+    private final JdbcTemplate jdbcTemplate;
+    private final UserRepository userRepository;
+
+    private User getAuthenticate() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String login = authentication.getName();
+        log.info("Token has been taken!");
+        return userRepository.findUserInfoByEmail(login).orElseThrow(() -> {
+            log.error("User not found!");
+            return new NotFoundException("User not found!");
+        }).getUser();
+    }
 
     @Override
     public SimpleResponse saveProduct(ProductRequest productRequest) {
@@ -51,5 +69,38 @@ public class ProductServiceImpl implements ProductService {
             subProductRepository.save(subProduct);
         }
         return SimpleResponse.builder().httpStatus(HttpStatus.OK).message("Successfully saved!!").build();
+    }
+
+    @Override
+    public List<CompareProductResponse> compare() {
+        String sql = """
+                SELECT (SELECT sci FROM sub_product_images sci where sci.sub_product_id = sp.id LIMIT 1) as image,(p.name) as name
+                     ,p.description as description,sp.price as price ,b.name as brand_name,
+                      (SELECT spc.characteristics from sub_product_characteristics spc where spc.characteristics_key='screen' and spc.sub_product_id = sp.id) as screen,
+                      sp.colour as color,
+                      (SELECT spc.characteristics from sub_product_characteristics spc where spc.characteristics_key='operatingSystem' and spc.sub_product_id = sp.id) as operatingSystem,
+                      (SELECT spc.characteristics from sub_product_characteristics spc where spc.characteristics_key='memory' and spc.sub_product_id = sp.id) as memory,
+                      (SELECT spc.characteristics from sub_product_characteristics spc where spc.characteristics_key='weight' and spc.sub_product_id = sp.id) as weight,
+                      (SELECT spc.characteristics from sub_product_characteristics spc where spc.characteristics_key='simCard' and spc.sub_product_id = sp.id) as simCard
+                              
+                FROM products p JOIN sub_products sp on p.id = sp.product_id
+                    JOIN users_comparisons uc on uc.comparisons_id = sp.id
+                    JOIN users u on uc.user_id = u.id JOIN brands b on p.brand_id = b.id where u.id = ?
+                 """;
+        return jdbcTemplate.query(sql, (resultSet, i) ->
+                new CompareProductResponse(
+                        resultSet.getString("image"),
+                        resultSet.getString("name"),
+                        resultSet.getString("description"),
+                        resultSet.getBigDecimal("price"),
+                        resultSet.getString("brand_name"),
+                        resultSet.getString("screen"),
+                        resultSet.getString("color"),
+                        resultSet.getString("operatingSystem"),
+                        resultSet.getString("memory"),
+                        resultSet.getString("weight"),
+                        resultSet.getString("simCard")
+                ), getAuthenticate().getId()
+        );
     }
 }
