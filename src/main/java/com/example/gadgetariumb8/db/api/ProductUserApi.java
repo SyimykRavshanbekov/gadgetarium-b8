45461dbda96fb0764962ad98a6a46f3172607c06
kutman <kkutman18@gmail.com>
2023-05-02 20:33:07 +0600

    fixed

--- src/main/java/com/example/gadgetariumb8/db/api/ProductUserApi.java
+++ src/main/java/com/example/gadgetariumb8/db/api/ProductUserApi.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.security.PermitAll;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
--- src/main/java/com/example/gadgetariumb8/db/service/ProductService.java
+++ src/main/java/com/example/gadgetariumb8/db/service/ProductService.java
@@ -4,13 +4,16 @@ import com.example.gadgetariumb8.db.dto.response.*;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 public interface ProductService {
 
     SimpleResponse saveProduct(ProductRequest productRequest);
 
-    CatalogResponse findByCategoryIdAndFilter(Long categoryId, Optional<Long> subCategoryId,
-
+   CatalogResponse findByCategoryIdAndFilter(Long categoryId, Optional<Long> subCategoryId,
+                                                     String[] brand, String priceFrom, String priceTo,
+                                                     String[] colour, String[] memory, String[] RAM, String[] watch_material,
+                                                     String gender, String sortBy, int pageSize);
     List<CompareProductResponse> compare();
 
     PaginationResponse<ProductsResponse> getAllDiscountProducts(int page, int pageSize);
