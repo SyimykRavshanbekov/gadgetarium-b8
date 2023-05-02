3feeed09e56ddefbdff5c581d4dc08443f292adc
kutman <kkutman18@gmail.com>
2023-05-02 20:33:07 +0600

    fixed

--- src/main/java/com/example/gadgetariumb8/db/service/ProductService.java
+++ src/main/java/com/example/gadgetariumb8/db/service/ProductService.java
@@ -3,11 +3,7 @@ package com.example.gadgetariumb8.db.service;
 import com.example.gadgetariumb8.db.dto.response.CatalogResponse;
 import java.util.Optional;
 import com.example.gadgetariumb8.db.dto.request.ProductRequest;
-import com.example.gadgetariumb8.db.dto.response.CompareProductResponse;
-import com.example.gadgetariumb8.db.dto.response.PaginationResponse;
-import com.example.gadgetariumb8.db.dto.response.ProductsResponse;
-import com.example.gadgetariumb8.db.dto.response.ProductAdminResponse;
-import com.example.gadgetariumb8.db.dto.response.SimpleResponse;
+import com.example.gadgetariumb8.db.dto.response.*;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -16,12 +12,10 @@ public interface ProductService {
 
     SimpleResponse saveProduct(ProductRequest productRequest);
 
-    CatalogResponse findByCategoryIdAndFilter(Long categoryId, Optional<Long> subCategoryId,
-                                              String[] category, String priceFrom,
-                                              String priceTo, String[] colour,
-                                              String[] memory, String[] RAM, String[] watch_material,
-                                              String gender, String sortBy, int pageSize);
-
+   CatalogResponse findByCategoryIdAndFilter(Long categoryId, Optional<Long> subCategoryId,
+                                                     String[] brand, String priceFrom, String priceTo,
+                                                     String[] colour, String[] memory, String[] RAM, String[] watch_material,
+                                                     String gender, String sortBy, int pageSize);
     List<CompareProductResponse> compare();
 
     PaginationResponse<ProductsResponse> getAllDiscountProducts(int page, int pageSize);
