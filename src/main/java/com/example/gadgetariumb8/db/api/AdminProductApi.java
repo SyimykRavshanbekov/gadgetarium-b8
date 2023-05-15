ed394b77bcf13d4ac7c67a3ccaf583812c04d189
Kurstan <kurstanerkinbaev2@gmail.com>
2023-05-15 21:07:46 +0300

    Added method product details to the AdminProductApi API and implemented in the SubProductService.

--- src/main/java/com/example/gadgetariumb8/db/api/AdminProductApi.java
+++ src/main/java/com/example/gadgetariumb8/db/api/AdminProductApi.java
@@ -3,26 +3,26 @@ package com.example.gadgetariumb8.db.api;
 import com.example.gadgetariumb8.db.dto.request.ProductRequest;
 import com.example.gadgetariumb8.db.dto.response.PaginationResponse;
 import com.example.gadgetariumb8.db.dto.response.ProductAdminResponse;
+import com.example.gadgetariumb8.db.dto.response.ProductDetailsResponse;
 import com.example.gadgetariumb8.db.dto.response.SimpleResponse;
-import com.example.gadgetariumb8.db.dto.response.SubProductResponse;
 import com.example.gadgetariumb8.db.service.ProductService;
 import com.example.gadgetariumb8.db.service.SubProductService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.Past;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/admin/products")
 @RequiredArgsConstructor
 @Tag(name = "Product Admin API")
 @CrossOrigin(origins = "*", maxAge = 3600)
-@PreAuthorize("hasAuthority('ADMIN')")
+//@PreAuthorize("hasAuthority('ADMIN')")
 public class AdminProductApi {
     private final ProductService productService;
     private final SubProductService subProductService;
@@ -44,10 +44,11 @@ public class AdminProductApi {
                                                            @RequestParam(required = false, defaultValue = "7") int pageSize) {
         return productService.getAll(keyWord, status, from, before, sortBy, page, pageSize);
     }
-    @GetMapping("/last_views")
-    @Operation(summary = "Last viewed products ", description = "This method shows the last 7 items viewed")
-    @PreAuthorize("hasAuthority('USER')")
-    public PaginationResponse<SubProductResponse> findAllSubProductLastViews(@RequestParam(defaultValue = "1") int page, @RequestParam(defaultValue = "5") int pageSize){
-        return subProductService.lastViews(page, pageSize);
+
+    @GetMapping("/{id}/product_details")
+    @Operation(summary = "Get product details", description = "This method to get product details by product id")
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
+    public List<ProductDetailsResponse> productDetails(@PathVariable("id") Long productId) {
+        return subProductService.getProductDetails(productId);
     }
 }
--- src/main/java/com/example/gadgetariumb8/db/service/SubProductService.java
+++ src/main/java/com/example/gadgetariumb8/db/service/SubProductService.java
@@ -6,7 +6,10 @@ import java.util.List;
 
 public interface SubProductService {
     PaginationResponse<SubProductResponse> lastViews(int page, int pageSize);
-    PaginationResponse<SubProductBasketResponse>getAllBasket(int page, int pageSize);
-    SimpleResponse deleteOrMoveToFavorites(String key,List<Long>longs);
 
+    PaginationResponse<SubProductBasketResponse> getAllBasket(int page, int pageSize);
+
+    SimpleResponse deleteOrMoveToFavorites(String key, List<Long> longs);
+
+    List<ProductDetailsResponse> getProductDetails(Long productId);
 }
