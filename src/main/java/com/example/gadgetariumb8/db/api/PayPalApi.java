020f09d72c5c6055e7803ebf0b20f1c2e0856e5b
kutman <kkutman18@gmail.com>
2023-05-22 17:09:30 +0600

    commit fixed

--- src/main/java/com/example/gadgetariumb8/db/api/PayPalApi.java
+++ src/main/java/com/example/gadgetariumb8/db/api/PayPalApi.java
@@ -1,16 +1,10 @@
 package com.example.gadgetariumb8.db.api;
 
-import com.example.gadgetariumb8.db.dto.request.OrderPaymentRequest;
 import com.example.gadgetariumb8.db.dto.response.SimpleResponse;
 import com.example.gadgetariumb8.db.service.PayPalService;
-import com.paypal.api.payments.Links;
-import com.paypal.api.payments.Payment;
-import com.paypal.base.rest.PayPalRESTException;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,24 +21,6 @@ public class PayPalApi {
     @Operation(summary = "Creating a payment",
             description = "This method creates payment with paypal system.")
     public SimpleResponse successPay(@RequestParam("paymentId") String paymentId, @RequestParam("PayerID") String payerId) {
-        try {
-            Payment payment = service.executePayment(paymentId, payerId);
-            System.out.println(payment.toJSON());
-            if (payment.getState().equals("approved")) {
-                return SimpleResponse
-                        .builder()
-                        .httpStatus(HttpStatus.OK)
-                        .message("Payment Success")
-                        .build();
-            }
-        } catch (PayPalRESTException e) {
-            System.out.println(e.getMessage());
-        }
-        return SimpleResponse
-                .builder()
-                .httpStatus(HttpStatus.BAD_REQUEST)
-                .message("Payment Failure")
-                .build();
+        return service.successPay(paymentId, payerId);
     }
-
 }
--- src/main/java/com/example/gadgetariumb8/db/config/payment/PaypalConfig.java
+++ src/main/java/com/example/gadgetariumb8/db/config/payment/PaypalConfig.java
@@ -3,6 +3,7 @@ package com.example.gadgetariumb8.db.config.payment;
 import com.paypal.base.rest.APIContext;
 import com.paypal.base.rest.OAuthTokenCredential;
 import com.paypal.base.rest.PayPalRESTException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,20 +12,23 @@ import java.util.Map;
 
 @Configuration
 public class PaypalConfig {
-    private static final String CLIENT_ID = "AYSq3RDGsmBLJE-otTkBtM-jBRd1TCQwFf9RGfwddNXWz0uFU9ztymylOhRS";
-    private static final String CLIENT_SECRET = "EGnHDxD_qRPdaLdZz8iCr8N7_MzF-YHPTkjs6NKYQvQSBngp4PTTVWkPZRbL";
-    private static final String MODE = "sandbox";
+    @Value("${paypal.client.id}")
+    private String clientId;
+    @Value("${paypal.client.secret}")
+    private String clientSecret;
+    @Value("${paypal.mode}")
+    private String mode;
 
     @Bean
     public Map<String, String> paypalSdkConfig() {
         Map<String, String> configMap = new HashMap<>();
-        configMap.put("mode", MODE);
+        configMap.put("mode", mode);
         return configMap;
     }
 
     @Bean
     public OAuthTokenCredential oAuthTokenCredential() {
-        return new OAuthTokenCredential(CLIENT_ID, CLIENT_SECRET, paypalSdkConfig());
+        return new OAuthTokenCredential(clientId, clientSecret, paypalSdkConfig());
     }
 
     @Bean
